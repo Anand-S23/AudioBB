32d1909bfb9f88d52ee83ff806761b5e0c608a51
Anand-S23 <anandsroopsingh23@gmail.com>
2021-10-26 22:49:21 -0400

    Create a book list with 10 elements in main activity

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -7,5 +7,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bookList: BookList = generateBookList()
+    }
+
+    private fun generateBookList() : BookList {
+        var bookList: BookList = BookList()
+
+        bookList.add(Book("The Lord of the Rings", "J. R. R. Tolkien"))
+        bookList.add(Book("Assassin's Apprentice", "Robin Hobb"))
+        bookList.add(Book("Dune", "Frank Herbert"))
+        bookList.add(Book("1984", "George Orwell"))
+        bookList.add(Book("The Way of the Kings", "Brandon Sanderson"))
+        bookList.add(Book("The Wheel of Time", "Robert Jordan"))
+        bookList.add(Book("Ender's Game", "Orson Scott Card"))
+        bookList.add(Book("Neuromancer", "William Gibson"))
+        bookList.add(Book("Red Rising", "Pierce Brown"))
+        bookList.add(Book("American Gods", "Neil Gaiman"))
+        bookList.add(Book("Harry Potter", "J.K. Rowling"))
+
+        return bookList
     }
 }
