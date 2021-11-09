ef5f105d80039cfa44689d5b59161e9c040d8bd6
Anand-S23 <anandsroopsingh23@gmail.com>
2021-11-09 13:00:33 -0500

    Remove generateBook function

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListInterface {
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container1, BookListFragment.newInstance(generateBookList()))
+                // .replace(R.id.container1, BookListFragment.newInstance(generateBookList()))
                 .commit()
         }
 
@@ -53,24 +53,6 @@ class MainActivity : AppCompatActivity(), BookListFragment.BookListInterface {
                 .addToBackStack(null)
                 .commit()
         }
-    }
-
-    private fun generateBookList() : BookList {
-        val bookList = BookList()
-
-        bookList.add(Book("The Lord of the Rings", "J. R. R. Tolkien"))
-        bookList.add(Book("Assassin's Apprentice", "Robin Hobb"))
-        bookList.add(Book("Dune", "Frank Herbert"))
-        bookList.add(Book("1984", "George Orwell"))
-        bookList.add(Book("The Way of the Kings", "Brandon Sanderson"))
-        bookList.add(Book("The Wheel of Time", "Robert Jordan"))
-        bookList.add(Book("Ender's Game", "Orson Scott Card"))
-        bookList.add(Book("Neuromancer", "William Gibson"))
-        bookList.add(Book("Red Rising", "Pierce Brown"))
-        bookList.add(Book("American Gods", "Neil Gaiman"))
-        bookList.add(Book("Harry Potter", "J.K. Rowling"))
-
-        return bookList
     }
 
     private fun bookNullOrBlank(_book: Book?) : Boolean {
